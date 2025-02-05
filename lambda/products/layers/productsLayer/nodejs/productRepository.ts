957fb38ba03917f969a6ef2e70ac56c630fed401
Evelyn Cristini Oliveira Throssell <cristinioliveira.eo@gmail.com>
2025-02-04 21:55:13 -0300

    feat: product interface and getAllProducts method in product repository

---
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -0,0 +1,27 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb"
+import { v4 as uuid } from "uuid"
+
+export interface Product {
+    id: string;
+    productName: string;
+    code: string;
+    price: number;
+    model: string
+}
+
+export class ProductRepository {
+    private ddbClient: DocumentClient
+    private productDdb: string
+
+    constructor(ddbClient: DocumentClient, productDdb: string) {
+        this.ddbClient = ddbClient
+        this.productDdb = productDdb
+    }
+
+    async getAllProducts(): Promise<Product[]> {
+        const data = await this.ddbClient.scan({
+            TableName: this.productDdb
+        }).promise()
+        return data.Items as Product[]
+    }
+}
